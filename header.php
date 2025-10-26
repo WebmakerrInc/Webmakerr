d32125de43cfba48cc9d2e1eb77d7715eadfc1a3
taskpul <romtietz@gmail.com>
2025-10-26 19:50:10 +0100

    Ensure contact button shows on mobile

--- header.php
+++ header.php
@@ -41,23 +41,23 @@
             </div>
 
             <div class="flex items-center gap-4">
-                <?php if (has_nav_menu('primary')): ?>
-                    <div class="flex items-center gap-3 md:hidden">
+                <div class="flex items-center gap-3 md:hidden">
+                    <?php if (has_nav_menu('primary')): ?>
                         <button type="button" aria-label="Toggle navigation" id="primary-menu-toggle">
                             <svg xmlns="https://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>
                         </button>
+                    <?php endif; ?>
 
-                        <a
-                            class="inline-flex items-center justify-center rounded-full bg-dark px-4 py-1.5 text-sm font-semibold text-white transition hover:bg-dark/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-dark !no-underline"
-                            href="<?php echo esc_url(home_url('/contact')); ?>"
-                            aria-label="<?php esc_attr_e('Contact Us', 'webmakerr'); ?>"
-                        >
-                            <?php esc_html_e('Contact Us', 'webmakerr'); ?>
-                        </a>
-                    </div>
-                <?php endif; ?>
+                    <a
+                        class="inline-flex items-center justify-center rounded-full bg-dark px-4 py-1.5 text-sm font-semibold text-white transition hover:bg-dark/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-dark !no-underline md:hidden"
+                        href="<?php echo esc_url(home_url('/contact')); ?>"
+                        aria-label="<?php esc_attr_e('Contact Us', 'webmakerr'); ?>"
+                    >
+                        <?php esc_html_e('Contact Us', 'webmakerr'); ?>
+                    </a>
+                </div>
 
                 <div id="primary-navigation" class="hidden flex flex-col gap-6 items-stretch border border-light rounded-xl p-4 md:flex md:flex-row md:items-center md:border-none md:bg-transparent md:p-0">
                     <nav>
